a082792982b91c2b0de424698e8ad92338a7afe3
Jinshu Liu <2532945491@qq.com>
2022-12-13 22:34:56 -0500

    Add install_qemu.sh

---
+++ install_qemu.sh
@@ -0,0 +1,26 @@
+sudo apt-get update
+
+sudo apt-get install -y git libglib2.0-dev libfdt-dev libpixman-1-dev zlib1g-dev ninja-build
+sudo apt-get install -y libslirp0 libslirp-dev
+
+# Clone QEMU
+git clone https://github.com/qemu/qemu.git
+
+# Switch to the QEMU root directory.
+cd qemu
+# Change version
+git checkout v7.2.0-rc4
+
+# Prepare a native debug build.
+mkdir -p bin/debug/native
+cd bin/debug/native
+# Configure QEMU and start the build.
+../../../configure --enable-debug
+make -j 32
+# Return to the QEMU root directory.
+cd ../../..
+
+# Configure QEMU for x86_64 only - faster build
+./configure --target-list=x86_64-softmmu --enable-debug
+# Build in parallel - my system has 4 CPUs
+make -j 32
