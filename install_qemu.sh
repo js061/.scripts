a05656306d4a71a3203da3ff0fb5fe29f74618bc
jsliu <55649996+js061@users.noreply.github.com>
2023-01-31 20:49:52 -0500

    Update install_qemu.sh

--- install_qemu.sh
+++ install_qemu.sh
@@ -1,7 +1,12 @@
 sudo apt-get update
 
 sudo apt-get install -y git libglib2.0-dev libfdt-dev libpixman-1-dev zlib1g-dev ninja-build
+
+# Enable slirp
 sudo apt-get install -y libslirp0 libslirp-dev
+
+# Enable NUMA node binding
+sudo apt-get install -y libnuma-dev
 
 # Clone QEMU
 git clone https://github.com/qemu/qemu.git
@@ -12,13 +17,13 @@ cd qemu
 git checkout v7.2.0
 
 # Prepare a native debug build.
-mkdir -p bin/debug/native
-cd bin/debug/native
+#mkdir -p bin/debug/native
+#cd bin/debug/native
 # Configure QEMU and start the build.
-../../../configure --enable-debug
-make -j 32
+#../../../configure --enable-debug
+#make -j 32
 # Return to the QEMU root directory.
-cd ../../..
+#cd ../../..
 
 # Configure QEMU for x86_64 only - faster build
 ./configure --target-list=x86_64-softmmu --enable-debug
